3295e1ba8752cd7faa30c4b8dd227482f651f387
Chris Hyde <c_hyde333@hotmail.com>
2022-09-06 14:06:57 +0100

    Setting up password section

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+const assets = {
+  images: {
+    logoDark: require("./images/logodark.png"),
+    appIcon: require("./images/favicon.png"),
+  },
+  colors: {
+    secondary: "#292929",
+  },
+};
+
+export default assets;
