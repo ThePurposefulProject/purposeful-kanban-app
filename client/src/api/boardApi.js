ee99bee2a1803fa7064fd8a9e485ab57268d2833
Chris Hyde <c_hyde333@hotmail.com>
2022-09-09 15:24:24 +0100

    Sidebar movement

--- client/src/api/boardApi.js
+++ client/src/api/boardApi.js
@@ -3,6 +3,8 @@ import axiosClient from "./axiosClient";
 const boardApi = {
   create: () => axiosClient.post("boards"),
   getAll: () => axiosClient.get("boards"),
+  updatePositoin: (params) => axiosClient.put("boards", params),
+  getOne: (id) => axiosClient.get(`boards/${id}`),
 };
 
 export default boardApi;
--- client/src/pages/Board.jsx
+++ client/src/pages/Board.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from'@mui/material'
+import boardApi from '../api/boardApi'
 
 const Board = () => {
+    const { boardId } = useParams()
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [sections, setSections] = useState([])
+    const [isFavourite, setIsFavoutite] = useState(false)
+    const [icon, setIcon] = useState('')
+
+    useEffect(() => {
+        const getBoard = async () => {
+            try {
+              const res = await boardApi.getOne(boardId)
+              setTitle(res.title)
+              setDescription(res.description)
+              setSections(res.sections)
+              setIsFavoutite(res.favourite)
+              setIcon(res.icon)
+            } catch (err) {
+                alert(err)
+            }
+        }
+        getBoard()
+    }, [boardId])
     return (
-        <div>Board</div>
+        <>
+        <Box>
+
+        </Box>
+        </>
     )
 }
 
