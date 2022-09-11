684c2cc9bf9d896fe7eef365912292a311dd244e
Chris Hyde <c_hyde333@hotmail.com>
2022-09-11 22:07:50 +0100

    Kanban working

--- server/src/v1/controllers/task.js
+++ server/src/v1/controllers/task.js
@@ -8,7 +8,7 @@ exports.create = async (req, res) => {
     const tasksCount = await Task.find({ section: sectionId }).count();
     const task = await Task.create({
       section: sectionId,
-      position: taskCount > 0 ? taskCount : 0,
+      position: tasksCount > 0 ? tasksCount : 0,
     });
     task._doc.section = section;
     res.status(201).json(task);
@@ -32,11 +32,47 @@ exports.delete = async (req, res) => {
   try {
     const currentTask = await Task.findById(taskId);
     await Task.deleteOne({ _id: taskId });
-    const tasks = await Task.find({ section: currentTask.section });
+    const tasks = await Task.find({ section: currentTask.section }).sort(
+      "postition"
+    );
     for (const key in tasks) {
       await Task.findByIdAndUpdate(tasks[key].id, { $set: { position: key } });
     }
     res.status(200).json("deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.updatePosition = async (req, res) => {
+  const {
+    resourceList,
+    destinationList,
+    resourceSectionId,
+    destinationSectionId,
+  } = req.body;
+  const resourceListReverse = resourceList.reverse();
+  const destinationListReverse = destinationList.reverse();
+  try {
+    if (resourceSectionId !== destinationSectionId) {
+      for (const key in resourceListReverse) {
+        await Task.findByIdAndUpdate(resourceListReverse[key].id, {
+          $set: {
+            section: resourceSectionId,
+            position: key,
+          },
+        });
+      }
+    }
+    for (const key in destinationListReverse) {
+      await Task.findByIdAndUpdate(destinationListReverse[key].id, {
+        $set: {
+          section: destinationSectionId,
+          position: key,
+        },
+      });
+    }
+    res.status(200).json("updated");
   } catch (err) {
     res.status(500).json(err);
   }
